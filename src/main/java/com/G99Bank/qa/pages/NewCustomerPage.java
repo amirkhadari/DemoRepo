640bae81a6b3e9af36fa5ccdfc9cf59df8a9c646
Sonali Agarwal <sonali.agarwal@innoraft.com>
2019-03-28 15:26:27 +0530

    Added data driven approach in framework

--- src/main/java/com/G99Bank/qa/pages/NewCustomerPage.java
+++ src/main/java/com/G99Bank/qa/pages/NewCustomerPage.java
@@ -3,6 +3,7 @@ package com.G99Bank.qa.pages;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
 
 import com.G99Bank.qa.base.TestBase;
 
@@ -10,6 +11,32 @@ public class NewCustomerPage extends TestBase {
 	
 	@FindBy(xpath = "//p[text()='Add New Customer']")
 	WebElement AddNewCustomerText;
+	
+	@FindBy(xpath = "//input[@name='name']")
+	WebElement custname;
+	@FindBy(xpath ="//input[@value='f']")
+	WebElement female;
+	@FindBy(id="dob")
+	WebElement birth;
+	@FindBy(xpath="//textarea[@name='addr']")
+	WebElement address;
+	@FindBy(xpath = "//input[@name='city']")
+	WebElement city;
+	@FindBy(xpath = "//input[@name='state']")
+	WebElement state;
+	@FindBy(xpath = "//input[@name='pinno']")
+	WebElement pinno;
+	@FindBy(xpath = "//input[@name='telephoneno']")
+	WebElement telephoneno;
+	@FindBy(xpath = "//input[@name='emailid']")
+	WebElement emailid;
+	@FindBy(xpath = "//input[@name='password']")
+	WebElement password;
+	@FindBy(xpath = "//input[@name='sub']")
+	WebElement sub;
+	
+	
+	
 	
 	public NewCustomerPage() {
 		PageFactory.initElements(driver, this);
@@ -23,5 +50,23 @@ public class NewCustomerPage extends TestBase {
 		return driver.getTitle();
 	}
 	
+	public void CreateNewCustomer(String name, String gender, String db, String adrs,
+			String cit, String sta, String pno, String tno, String id, String pwd) {
+		
+		custname.sendKeys(name);
+		if(gender.equals("f")) {
+			female.click();
+		}
+		birth.sendKeys(db);
+		address.sendKeys(adrs);
+		city.sendKeys(cit);
+		state.sendKeys(sta);
+		pinno.sendKeys(pno);
+		telephoneno.sendKeys(tno);
+		emailid.sendKeys(id);
+		password.sendKeys(pwd);
+		
+		sub.click();
+	}
 
 }
--- src/test/java/com/G99Bank/qa/Testcases/NewCustomerPageTest.java
+++ src/test/java/com/G99Bank/qa/Testcases/NewCustomerPageTest.java
@@ -1,20 +1,27 @@
 package com.G99Bank.qa.Testcases;
 
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import com.G99Bank.qa.base.TestBase;
 import com.G99Bank.qa.pages.HomePage;
 import com.G99Bank.qa.pages.LogIn;
 import com.G99Bank.qa.pages.NewCustomerPage;
+import com.G99Bank.qa.test.Xls_Reader1;
+import com.G99Bank.qa.util.TestUtil;
 
 public class NewCustomerPageTest extends TestBase{
 	
 	LogIn login;
 	HomePage homepage;
 	NewCustomerPage newcustomer;
+	
+	static String sheetName = "Sheet2";
+	
 	public NewCustomerPageTest() {
 		super();
 	}
@@ -28,7 +35,7 @@ public class NewCustomerPageTest extends TestBase{
 		login.login(prop.getProperty("userid"), prop.getProperty("password"));
 		homepage.clickOnNewCustomerLink();
 	}
-	@Test(priority=1)
+	/*@Test(priority=1)
 	public void VerifyNewCustomerTitleTest() {
 		String title = newcustomer.NewCustomerTitle();
 		Assert.assertEquals(title,"Guru99 Bank New Customer Entry Page");
@@ -38,9 +45,23 @@ public class NewCustomerPageTest extends TestBase{
 	public void verifyAddCustomertextTest() {
 		boolean b = newcustomer.AddCustomerText();
 		Assert.assertTrue(b, "add customer text is not displayed");
+	}*/
+	
+	@DataProvider
+	public Object[][] getNewCustomerData() {
+		Object[][] data = TestUtil.getTestData(sheetName);
+		return data;
 	}
 	
-	
+	@Test(priority=3, dataProvider = "getNewCustomerData")
+	public void validateNewCustomerForm(String customername, String gen, String DoB, String Addr, 
+			String CITY, String STATE, String pinnumber, String phonenumber, String mailid, String pd) {
+		
+//		newcustomer.CreateNewCustomer("amirsyed", "m", "22/11/1997", "Jaipur", "Jaipur", "Rajasthan",
+//				"302017", "9065781234", "amir.syed@gmail.com", "amirsyed123");
+		newcustomer.CreateNewCustomer(customername, gen, DoB, Addr, CITY, STATE, pinnumber, phonenumber, mailid, pd);
+		
+	}
 	
 	@AfterMethod()
 	public void tearDown() {
